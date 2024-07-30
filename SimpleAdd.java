89c2cd2b6523bd08f27c92d0730d0638f441615e
priyadharshini2025 <skmp47200@gmail.com>
2024-07-30 23:04:15 +0530

    Add files via upload

---
+++ SimpleAdd.java
@@ -0,0 +1,10 @@
+public class SimpleAdd {
+    public static void main(String[] args) {
+        int num1 = 5; // first number
+        int num2 = 10; // second number
+
+        int sum = num1 + num2; // adding the two numbers
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
